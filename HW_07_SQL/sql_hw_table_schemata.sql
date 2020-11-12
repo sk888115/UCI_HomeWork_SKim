326ca2c374855509643dfbe7eb7ece69cb0006c5
sk888115 <70560802+sk888115@users.noreply.github.com>
2020-11-11 22:09:33 -0800

    updated readme.md

--- HW_07_SQL/sql_hw_table_schemata.sql
+++ HW_07_SQL/sql_hw_table_schemata.sql
@@ -3,25 +3,25 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
+CREATE TABLE "departments" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "dept_name" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
+    CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
-CREATE TABLE "Dept_Manager" (
+CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "emp_no" INT(10)   NOT NULL
 );
 
-CREATE TABLE "Dept_Emp" (
+CREATE TABLE "dept_emp" (
     "emp_no" INT(10)   NOT NULL,
     "dept_no" VARCHAR(10)   NOT NULL
 );
 
-CREATE TABLE "Employees" (
+CREATE TABLE "employees" (
     "emp_no" INT(10)   NOT NULL,
     "emp_title" VARCHAR(10)   NOT NULL,
     "birth_date" VARCHAR(10)   NOT NULL,
@@ -29,39 +29,39 @@ CREATE TABLE "Employees" (
     "last_name" VARCHAR(30)   NOT NULL,
     "sex" VARCHAR(10)   NOT NULL,
     "hire_date" VARCHAR(10)   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" INT(10)   NOT NULL,
     "salary" INT(10)   NOT NULL
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "title_id" VARCHAR(10)   NOT NULL,
     "title" VARCHAR(10)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
+    CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_Manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_Manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_Emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title" FOREIGN KEY("emp_title")
-REFERENCES "Titles" ("title_id");
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
