62395e7e4565056ddd91eb3702494aa296cc3a37
sk888115 <70560802+sk888115@users.noreply.github.com>
2020-11-11 22:19:14 -0800

    query update

--- HW_07_SQL/sql_hw_query.sql
+++ HW_07_SQL/sql_hw_query.sql
@@ -4,13 +4,13 @@ FROM employees
 JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
--- 2. List employees who were hired in 1986.
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date 
 FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
-SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name, dept_manager.from_date, dept_manager.to_date
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 FROM departments
 JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no
@@ -25,8 +25,8 @@ ON dept_emp.emp_no = employees.emp_no
 JOIN departments
 ON dept_emp.dept_no = departments.dept_no;
 
--- 5. List all employees whose first name is "Hercules" and last names begin with "B."
-SELECT first_name, last_name
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
@@ -40,7 +40,7 @@ JOIN departments
 ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales';
 
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 7/ List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
 JOIN employees
