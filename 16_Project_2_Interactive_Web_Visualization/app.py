2ea497e4e54cc2fd05fca6caf2a04470252c3ee1
sk888115 <70560802+sk888115@users.noreply.github.com>
2021-01-24 12:15:42 -0800

    group project 2

---
+++ 16_Project_2_Interactive_Web_Visualization/app.py
@@ -0,0 +1,87 @@
+# import dependencies
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+from config import pg_password
+
+#####################################################
+# Database Setup
+#####################################################
+
+# Create connection string to postgres
+connection_string = f"postgres:{pg_password}@localhost:5432/covid_mask_effect_db"
+engine = create_engine(f'postgresql://{connection_string}')
+
+# Reflect database into a new model using Base = automap()
+Base = automap_base()
+
+# Setup .prepare(engine, reflect=True) to reflect tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to california_data, nocal_data, and socal_data tables
+cali_data = Base.classes.cali_data
+nocal_data = Base.classes.nocal_data
+socal_data = Base.classes.socal_data
+
+#######################################################
+# Setup Flask
+#######################################################
+
+# Initiate Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    # Create connection
+    session = Session(engine)
+    # california_data
+    cali_counties = session.query(cali_data.county).all()
+    cali_cases = session.query(cali_data.cases).all()
+    cali_dates = session.query(cali_data.date).all()
+
+    # nocal_data
+    nocal_counties = session.query(nocal_data.county).all()
+    nocal_cases = session.query(nocal_data.cases).all()
+    nocal_dates = session.query(nocal_data.date).all()
+
+    # socal_data
+    socal_counties = session.query(socal_data.county).all()
+    socal_cases = session.query(socal_data.cases).all()
+    socal_dates = session.query(socal_data.date).all()
+
+    # return jsonify({"North Cali County": nocal_counties}, {"North Cali Cases": nocal_cases}, {"North Cali Dates": nocal_dates}, {"So Cal County": socal_counties}, {" So Cal Cases": socal_cases}, {"So Cal Dates": socal_dates})
+    return jsonify({"county" : socal_dates})
+# @app.route('/test', methods=['GET', 'POST'])
+# def testfn():
+
+#     # Get request
+#     if request.method == 'GET':
+#         message = {'greeting':'Hello from Flask!'}
+#         return jsonify(message)
+
+#     if request.method == 'POST':
+#         print(request.get_json())
+#         return 'Sucess', 200
+
+
+# def homepage():
+
+# @app.route("/api/v1.0/calendar")
+# def calendar():
+
+# @app.route("/api/v1.0/map")
+# def map():
+
+# @app.route("/api/v1.0/scatter")
+# def scatter():
+
+# @app.route("/api/v1.0/")
+
+
+
+# Run app
+if __name__ == "__main__":
+    app.run(debug=True) 
